476bae347fe39f7da4fed00a1acd0d1507c623b4
Dmitry <Dololob92@gmail.com>
2025-11-08 21:42:25 +0300

    fix: error

--- src/methods/GET.ts
+++ src/methods/GET.ts
@@ -1,10 +1,11 @@
 import { store } from '../store/store.ts';
 import type { IResponse } from '../types/types.ts';
+import { validate } from 'uuid';
 
 export const GET = (url: string | undefined) => {
   const response: Partial<IResponse> = {};
   const userId = url?.split('/')[3];
-  if (url && userId) {
+  if (userId && validate(userId)) {
     for (let i = 0; i < store.length; i++) {
       if (userId === store[i].id.toString()) {
         response.status = 200;
@@ -14,11 +15,15 @@ export const GET = (url: string | undefined) => {
     }
 
     response.status = 404;
-    response.data = 'Invalid userId';
+    response.data = 'This user does not exist';
     return response;
-  } else {
+  } else if (!userId) {
     response.status = 200;
     response.data = store;
+    return response;
+  } else {
+    response.status = 400;
+    response.data = 'Invalid userID';
     return response;
   }
 };
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,3 @@
 import type { IStore } from '../types/types';
 
-export const store: IStore[] = [
-  {
-    id: '15',
-    username: 'Chel_1',
-    age: 45,
-    hobbies: 'sport',
-  },
-  {
-    id: '23',
-    username: 'Chel_2',
-    age: 33,
-    hobbies: 'music',
-  },
-];
+export const store: IStore[] = [];
